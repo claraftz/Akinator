d147cbf832ac6ffb8c7dd66abb0f8849ceadb931
Clara Fritz <clara.fritz@3wa.io>
2026-02-18 22:17:39 +0000

    Ajout du typage de la fonction

--- connectToDb.php
+++ connectToDb.php
@@ -1,6 +1,6 @@
 <?php
-
-function connectToDb() {
+declare(strict_types=1);
+function connectToDb() : PDO {
     $dsn = 'mysql:host=db.3wa.io;port=3306;dbname=clarafritz_sprint_akinator;charset=utf8';
     $user = 'clarafritz';
     $password = '7aecfc5cd604a4900c524a7a25615655';
