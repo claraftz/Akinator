bb19fa5887a0068e184e999dd36a8652c1367756
Clara Fritz <clara.fritz@3wa.io>
2026-02-18 12:57:02 +0000

    Authentification : Gestion de la connexion et inscription

---
+++ connection.php
@@ -0,0 +1,61 @@
+<?php 
+
+session_start(); 
+require_once 'connectToDb.php';
+
+$error = '';
+
+
+/**
+ * 1. Appelle fonction pr se connecter a la Db sql 
+ * 2. Verif des champs si remplit ou non 
+ * 3. Recherche de l'user / player par son mail
+ * 4. Verif de l'user +  son mdp 
+ * 5. Connexion reussi : on stocke les infos (session)
+ * 6. Redirection page profil / home
+**/
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        //1. 
+        $db = connectToDb(); 
+        //2.  
+        if (empty($_POST['email']) || empty($_POST['password'])) {
+            throw new Exception('Veuillez remplir tous les champs pour vous connecter Jedi !');
+        }
+        
+        //3. 
+        $research = $db->prepare('SELECT * FROM Player WHERE email = ?');
+        $research->execute([$_POST['email']]);
+        $user = $research->fetch(PDO::FETCH_ASSOC);
+        
+        
+        //4.
+        if(!$user || !password_verify($_POST['password'], $user['password'])) {
+            throw new Exception("Identifiants incorrects, ressaisis toi Jedi !");
+        }
+        
+        
+        //5.
+        $_SESSION['user'] = [
+            'id' => $user['id'],
+            'nickname' => $user['nickname'],
+            'email' => $user['email']
+        ];
+        
+        
+        //6. 
+        header('Location: profil.php');
+        exit;
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+}
+
+
+
+
+$title = 'Akinator - Connexion';
+$template = 'template/connection.phtml';
+include 'template/layout.phtml';
+
+?>
---
+++ registration.php
@@ -0,0 +1,76 @@
+<?php 
+session_start();
+require_once 'connectToDb.php';
+
+$error = '';
+
+/**
+ * 1. Connexion base donnée via fonction 
+ * 2. Verif champs vides d'inscription
+ * 3. Verif du format email
+ * 4. Verif du mdp + respecte le <p> (regex)
+ * 5. Verif si email existe deja dans Player 
+ * 6. Hachage mdp 
+ * 7. Insertion dans DB
+ * 8. succès -> redirection vers connexion 
+**/
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    
+    try{
+        //1.
+        $db = connectToDb();
+        
+        
+        //2.
+        if(empty($_POST['nickname']) || empty($_POST['email']) || empty($_POST['password'])) {
+            throw new Exception("Tous les champs sont obligatoires pour rejoindre l'Alliance !");
+        }
+        
+        
+        //3.
+        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Format d'email invalide !");
+        }
+        
+        
+        //4.
+        $password = $_POST['password'];
+        $regex = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/';
+        if(!preg_match($regex, $password)) {
+            throw new Exception("Le mot de passe ne respecte pas les critères de sécurité. Attention Jedi !");
+        }
+        
+        
+        //5.
+        $checkEmail = $db->prepare('SELECT id FROM Player WHERE email = ?');
+        $checkEmail->execute([$_POST['email']]);
+        if ($checkEmail->fetch()){
+            throw new Exception('Cet email est déjà utilisé par un autre Jedi !');
+        }
+        
+        
+        //6.
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        
+        
+        //7.
+        $insertIntoDb = $db->prepare('INSERT INTO Player (nickname, email, password) VALUES (?, ?, ?)');
+        $insertIntoDb->execute([
+            htmlspecialchars($_POST['nickname']),
+            $_POST['email'],
+            $passwordHash
+        ]);
+        
+        
+        //8.
+        header('Location: connection.php');
+        exit;
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+}
+
+
+$title = 'Akinator - Inscription';
+$template = 'template/registration.phtml';
+include 'template/layout.phtml';
